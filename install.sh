09aa29662bf5490ad356b4c8df769701c75fb59d
Brian Merritt <brian.merritt@jhuapl.edu>
2024-04-10 17:55:58 -0400

    update install

--- install.sh
+++ install.sh
@@ -3,14 +3,24 @@
 # Check for Conda installation
 if  ! command -v conda; then
     echo "Conda not found. Installing Miniconda..."
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
-    bash ~/miniconda.sh -b -u -p ~/miniconda3
-    rm ~/miniconda.sh
-    echo "Initializing Miniconda..."
-    ~/miniconda3/bin/conda init bash
+    # wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+    # bash ~/miniconda.sh -b -u -p ~/miniconda3
+    # rm ~/miniconda.sh
+    # echo "Initializing Miniconda..."
+    # ~/miniconda3/bin/conda init bash
 else
     echo "Conda is already installed."
 fi
+
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine="";;
+    Darwin*)    machine="CONDA_SUBDIR=osx064";;
+    *)          machine=""
+esac
+echo ${machine}
+
 $HOME/miniconda3/bin/conda init bash
 source ~/miniconda3/etc/profile.d/conda.sh
 
@@ -26,13 +36,7 @@ fi
 
 echo "Creating 'omics_workshop' environment..."
 
-unameOut="$(uname -s)"
-case "${unameOut}" in
-    Linux*)     machine="";;
-    Darwin*)    machine="CONDA_SUBDIR=osx064";;
-    *)          machine=""
-esac
-echo ${machine}
+
 
 $machine conda create -y -c bioconda -n omics_workshop bowtie2 minimap2 kraken2 krona fastqc samtools bcftools git python
 
