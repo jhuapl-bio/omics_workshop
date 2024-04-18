e04f554de0a185b3c2be979ad4a92ae21a607697
Brian Merritt <brian.merritt@jhuapl.edu>
2024-04-18 12:25:09 -0400

    install bundle

--- install_bundle.sh
+++ install_bundle.sh
@@ -1,6 +1,26 @@
 #!/bin/bash -i
 
-wget --no-check-certificate https://github.com/jhuapl-bio/omics_workshop/raw/main/install.sh \
-	-O install.sh \
-	&& bash install.sh
+cmd(){
+	wget --no-check-certificate https://github.com/jhuapl-bio/omics_workshop/raw/main/install.sh \
+		-O install.sh \
+		&& bash install.sh
+}
+
+
+# Detect OS and open a new terminal window running the specified command
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS - using AppleScript to open Terminal and execute the command
+    osascript <<EOF
+	tell application "Terminal"
+    do script "wget --no-check-certificate https://github.com/jhuapl-bio/omics_workshop/raw/main/install.sh -O install.sh && bash install.sh"
+    activate
+end tell
+EOF
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # Linux - using GNOME Terminal as an example
+	# try to run a command and if it fails then open a new terminal window
+    gnome-terminal -- /bin/bash -c "wget --no-check-certificate https://github.com/jhuapl-bio/omics_workshop/raw/main/install.sh -O install.sh && bash install.sh; exec bash"
+else
+    cmd
+fi
 
